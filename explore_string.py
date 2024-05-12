ae30322f64848a6466c134db0a9b0af51f5baec6
unknown <naimurrahman8392@gmail.com>
2024-05-12 20:39:00 +0600

    first commit

---
+++ explore_string.py
@@ -0,0 +1,16 @@
+name='shakib\'s khan'
+name2="shakib khan"
+print(name,name2)
+
+for char in name:
+    print(char)
+
+print(name2[3])
+print(name2[1:6])
+print(name2[-2])
+print(name2[::-1])
+
+#name2[0]='R'
+print(name2)
+if 'khan' in name2:
+    print('exists')
